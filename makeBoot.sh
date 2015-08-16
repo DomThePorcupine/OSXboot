4877b3301826703575faef39bf81a264bbedf7e9
dominic <dominic.dipasquale@gmail.com>
2015-08-16 18:01:41 -0400

    Cleaned up echo/tempdir

--- makeBoot.sh
+++ makeBoot.sh
@@ -1,27 +1,26 @@
 #! /bin/bash
-echo "----------------------------------------------------"
-echo "|Welcome, I will turn an ISO into a bootable drive!|"
-echo "----------------------------------------------------"
+printf "----------------------------------------------------\n"
+printf "|Welcome, I will turn an ISO into a bootable drive!|\n"
+printf "----------------------------------------------------\n"
 printf "Please enter the *FULL* path to the ISO file: "
 read ISOpath
-echo $ISOpath
-mkdir ~/xhjksawd
-hdiutil convert -format UDRW -o ~/xhjksawd/newFile.img $ISOpath
-mv ~/xhjksawd/newFile.img.dmg ~/xhjksawd/newFile.img
+mkdir ~/tempBootDir
+hdiutil convert -format UDRW -o ~/tempBootDir/newFile.img $ISOpath
+mv ~/tempBootDir/newFile.img.dmg ~/tempBootDir/newFile.img
 ##### File has been created
-echo "----------------------------------------"
-echo "Here is a list of your connected devices"
-echo "----------------------------------------"
+printf "----------------------------------------\n"
+printf "Here is a list of your connected devices\n"
+printf "----------------------------------------\n"
 diskutil list | grep -e '/dev\|#:\|0:'
 printf "Enter disk you wish to use (Ex. disk1): "
 read diskNum
 notThis='disk0'
 if [[ *"$diskNum"* == *"$notThis"* ]];then
-	echo "I will not allow you to do this to the internal drive on your computer!"
-	echo "Sorry friend if you REALLY wanna do this then you can do it by hand yourself"
+	printf "I will not allow you to do this to the internal drive on your computer!\n"
+	printf "Sorry friend if you REALLY wanna do this then you can do it by hand yourself\n"
 	exit
 fi
 diskutil unmountDisk $diskNum
-sudo dd if=~/xhjksawd/newFile.img of=/dev/r$diskNum bs=1m
+sudo dd if=~/tempBootDir/newFile.img of=/dev/r$diskNum bs=1m
 echo "Cleaning up..."
-rm -rf ~/xhjksawd
+rm -rf ~/tempBootDir
